cce821c65aae6d218fe1ed91a1ead7a9669cf7fd
Rafaela Dartora <siglyane@gmail.com>
2021-11-14 16:31:06 -0300

    Cria projeto springboot

---
+++ src/main/java/com/company/subscriptions/SubscriptionsApplication.java
@@ -0,0 +1,13 @@
+package com.company.subscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/subscriptions/SubscriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
